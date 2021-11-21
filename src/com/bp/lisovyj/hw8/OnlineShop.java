cf12a050d5d43700e5688a1cb405e107024d9bf3
Victor Lissoviy <victor.lissoviy.93@gmail.com>
2021-11-21 09:17:58 +0200

    fix hw8

--- src/com/bp/lisovyj/hw8/OnlineShop.java
+++ src/com/bp/lisovyj/hw8/OnlineShop.java
@@ -6,20 +6,39 @@ public class OnlineShop {
     public static void main(String[] args) {
         Auth auth = new Auth();
         Scanner in = new Scanner(System.in);
-        System.out.print("Введіть логін: ");
-        String login = in.nextLine();
-        System.out.print("Введіть пароль: ");
-        String password = in.nextLine();
-        System.out.print("Повторіть пароль: ");
-        String confirmPassword = in.nextLine();
-        auth.signUp(login, password, confirmPassword);
-        System.out.println("Нового користувача створено успішно\nАвторизація");
-        System.out.print("Введіть логін: ");
-        login = in.nextLine();
-        System.out.print("Введіть пароль: ");
-        password = in.nextLine();
-        auth.signIn(login, password);
-        System.out.println("Вітаємо вас " + login + " в системі!");
+        boolean error = false;
+        do{
+            System.out.print("Введіть логін: ");
+            String login = in.nextLine();
+            System.out.print("Введіть пароль: ");
+            String password = in.nextLine();
+            System.out.print("Повторіть пароль: ");
+            String confirmPassword = in.nextLine();
+            try{
+                auth.signUp(login, password, confirmPassword);
+                System.out.println("Нового користувача створено успішно\nАвторизація");
+                error = false;
+            }catch(WrongLoginException | WrongPasswordException e){
+                error = true;
+                System.out.println(e.getMessage());
+                System.out.println("Спробуйте ще раз");
+            }
+        }while(error);
+        do{
+            System.out.print("Введіть логін: ");
+            String login = in.nextLine();
+            System.out.print("Введіть пароль: ");
+            String password = in.nextLine();
+            try{
+                auth.signIn(login, password);
+                System.out.println("Вітаємо вас, " + login + ", в системі!");
+                error = false;
+            }catch(WrongLoginException | WrongPasswordException e){
+                error = true;
+                System.out.println(e.getMessage());
+                System.out.println("Спробуйте ще раз");
+            }
+        }while(error);
         in.close();
     }
 }
